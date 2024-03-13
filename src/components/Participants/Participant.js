939c67bec2096671840f137c1b3c9f148e86fcc7
Varvara Korableva <v.korableva1990@yandex.ru>
2024-03-13 09:33:23 +0200

    change text

--- src/components/Participants/Participant.js
+++ src/components/Participants/Participant.js
@@ -8,7 +8,12 @@ function Paticipant({item}) {
     //const img = 'https://plus.unsplash.com/premium_photo-1709993969997-6885e7466468?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8'
     return (
         <li className="paticipant-container">
-           <img className="paticipant-img" src={item.imgLink} alt={`Логотип компании ${item.companyName}`}></img>
+
+           {item.imgLink === ''?
+            <p className="paticipant-img">Участник скоро добавит свой логотип</p>
+            :
+            <img className="paticipant-img" src={item.imgLink} alt={`Логотип компании ${item.companyName}`}></img>
+           }
            <h3 className="paticipant-companyName-title">{item.companyName}</h3> 
            {item.description.length? 
            <p className="paticipant-companyName-description">{item.description}</p>
