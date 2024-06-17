4171c9b000b5e8712089cb6f5b9979d2cc40b23e
Varvara Korableva <v.korableva1990@yandex.ru>
2024-06-17 12:44:29 +0300

    changed scheduale

---
+++ src/components/Theater/TheaterHaifa.js
@@ -0,0 +1,11 @@
+const { RenderErrorBoundary } = require("react-router/dist/lib/hooks");
+
+function TheaterHaifa() {
+    render(
+        <section>
+            <h2>СПЕКТАКЛИ</h2>
+        </section>
+    )
+}
+
+export default TheaterHaifa
