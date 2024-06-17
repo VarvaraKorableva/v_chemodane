2db19eea5e9ec8a782e68ab825051b1bb3995b21
Varvara Korableva <v.korableva1990@yandex.ru>
2024-06-17 16:03:21 +0300

    changed data

--- src/components/Theater/OnePerformance.js
+++ src/components/Theater/OnePerformance.js
@@ -7,7 +7,7 @@ function OnePerformance({item}) {
             <p className='onePerformance__date'>{item.Date}</p>
             <Link to={item.AggregatorLink} className='onePerformance__link-btn'>Купить билет</Link>
             <div className='onePerformance__text-container'>
-                <h2 className='onePerformance__title'>{item.CompanyName}</h2>
+                <h2 className='onePerformance__title'>{item.Name}</h2>
                 <p className='onePerformance__description'>{item.Description}</p>
             </div>
 
