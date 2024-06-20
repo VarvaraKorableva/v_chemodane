9b44c8099507a1d9d334e4d0e7521aaa66afad88
Varvara Korableva <v.korableva1990@yandex.ru>
2024-06-20 15:35:07 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,16 @@
 import { FaInstagram, FaTelegram, FaFacebook } from 'react-icons/fa'
 import './Footer.css'
+import logoPic from '../../images/logoch.png'
 
 function Footer() {
     return (
         <footer className='footer'>
+          <img src={logoPic} alt='Картинка логотипа' className="header__logo__img"></img>
             <nav className="footer__nav">
+                <div className="footer__contacts-container">
+                    <p>Контакты: </p>
+                    <p className="footer__contacts-tel"> + 972 53-470-1675</p>
+                </div>
                 <ul className="footer__links-container">
                     <li className='footer__link'>
                       <a className='footer__contact-item-link' href='https://www.instagram.com/vchemodanefest/' target='_blank'>
@@ -23,6 +29,13 @@ function Footer() {
                     </li>
                 </ul>
             </nav>
+
+            <div className='footer-star'></div>
+            <div className='footer__second-orange-second-star'></div>
+            <div className='footer__third-orange-star'></div>
+            <div className='footer__fourth-orange-star'></div>
+            <div className='footer__fifth-orange-star'></div>
+  
         </footer>
     );
 }
