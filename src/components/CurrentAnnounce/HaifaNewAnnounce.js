60dc1bf08bed659e9101f054e41b54a59cb76d57
Varvara Korableva <v.korableva1990@yandex.ru>
2024-06-23 09:14:33 +0300

    stylrs

--- src/components/CurrentAnnounce/HaifaNewAnnounce.js
+++ src/components/CurrentAnnounce/HaifaNewAnnounce.js
@@ -4,9 +4,8 @@ import { HashLink as Link } from 'react-router-hash-link'
 function HaifaNewAnnounce() {
     return(
         <section className="hifa-announce__container">
-            <div className="newAnnounce__text-wrapper">
-                <h1 className="hifa-announce__title">
-                    <span className="hifa-announce__title-date">1-2 июля</span><br/> фестиваль <span className="hifa-announce__title-nowrap">"В чемодане"</span><br/> возвращается <span className="hifa-announce__title-nowrap">в Хайфу!</span></h1>
+            <div className="hifa__text-wrapper">
+                <h1 className="hifa-announce__title"><span className="hifa-announce__title-date">1-2 июля</span><br/> фестиваль <span className="hifa-announce__title-nowrap">"В чемодане"</span><br/> возвращается <span className="hifa-announce__title-nowrap">в Хайфу!</span></h1>
                 <div className="newAnnounce__btn-container">
                     <Link className="newAnnounce__btn-link" to='#festival'>О фестивале</Link>
                     <Link className="newAnnounce__btn-link" to='/haifa-2024-schedule'>Расписание</Link>{/* */}
