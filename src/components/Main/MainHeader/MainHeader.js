2632940a6e4b5e714d5e49a7307ae1313b728970
Varvara Korableva <v.korableva1990@yandex.ru>
2024-10-23 16:57:08 +0300

    header

--- src/components/Main/MainHeader/MainHeader.js
+++ src/components/Main/MainHeader/MainHeader.js
@@ -1,12 +1,17 @@
 import teamPic from '../../../images/mainPage/mainpicheader.png'
+import book from '../../../images/mainPage/book.png'
+import letters from '../../../images/mainPage/letters.png'
 import './MainHeader.css'
 
 function MainHeader() {
     return(
         <header className="main-header">
             <div className="main-header__nav-container">
+                
                 <div className="main-header-logo"></div>
-                {/*<div className="main-header-letters"></div> */}
+                
+                <img src={letters} className="main-header-letters"></img>
+                <img src={book} className="main-header-book"></img>
                 <nav className="main-header-container">
                     <ul className="main-header-ul-container">
                         <li className="main-header-nav-link">О фестивале</li>
@@ -18,14 +23,15 @@ function MainHeader() {
                         <li className="main-header-nav-link">Контакты</li>
                     </ul>
                 
-                <ul className="main-header-ul-order-container">
-                    <li className="main-header-nav-link">Подать заявку на маркет</li>
-                    <li className="main-header-nav-link">Предложить свой МК или спектакль</li>
-                </ul>
+                    <ul className="main-header-ul-order-container">
+                        <li className="main-header-order-link">Подать заявку на маркет</li>
+                        <li className="main-header-order-link">Предложить свой МК или спектакль</li>
+                    </ul>
                 </nav>
+                <button className="main-header-burger-btn"></button>
             </div>
             <div className="main-header__img-container">
-                <img src={teamPic}></img>
+                <img className="main-header__img" src={teamPic} alt='team picture'></img>
             </div>
             
         </header>
