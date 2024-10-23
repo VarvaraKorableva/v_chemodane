eba68ea17c3e3517db526e5cc3ce8a007bdf20ee
Varvara Korableva <v.korableva1990@yandex.ru>
2024-10-23 22:40:35 +0300

    about section

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 //MainPage
 import MainHeader from '../../components/Main/MainHeader/MainHeader'
+import MainAbout from '../../components/Main/MainAbout/MainAbout'
 
 function MainPage() {
     return(
         <>
             <MainHeader/>
+            <MainAbout/>
         </>
     )
 } 
