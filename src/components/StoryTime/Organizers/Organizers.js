da650acf3d2fab880a5baf560285eb8aeabf35b9
Varvara Korableva <v.korableva1990@yandex.ru>
2024-11-02 15:35:33 +0200

    styles

--- src/components/StoryTime/Organizers/Organizers.js
+++ src/components/StoryTime/Organizers/Organizers.js
@@ -1,5 +1,5 @@
 import './Organizers.css'
-import logoVChemodame from '../../../images/logo/logovchemodane.png'
+import logoVChemodame from '../../../images/mainPage/logobottom.png'
 import ujFederation from '../../../images/logo/ujFederation.png'
 import Nadav from '../../../images/logo/Nadav.png'
 
@@ -8,13 +8,13 @@ function Organizers() {
         <section className='organizers__section' id='Organizers'>
             <h2 className='organizers__title'>ОРГАНИЗАТОРЫ</h2>
             <ul className='organizers__ul-wrapper'>
-                <li className='organizers__li-containerVChemodame'>
+                <li className='organizers__li-container organizers__li-containerlogoVChemodame'>
                     <img src={logoVChemodame} alt="Логотип Фестиваля ВЧемодане" className='organizers__logo'></img>
                 </li>
                 <li className='organizers__li-container'>
                     <img src={ujFederation} alt="Логотип UJ Federation" className='organizers__logo'></img>
                 </li>
-                <li className='organizers__li-containerNadav'>
+                <li className='organizers__li-container'>
                     <img src={Nadav} alt="Логотип Nadav" className='organizers__logo'></img>
                 </li>
             </ul>
