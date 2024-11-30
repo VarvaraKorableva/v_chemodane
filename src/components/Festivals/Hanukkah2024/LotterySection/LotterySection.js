6d0654494ba6860d74a8102e1c5ba8bedcdad521
Varvara Korableva <v.korableva1990@yandex.ru>
2024-11-30 17:22:54 +0200

    styles

--- src/components/Festivals/Hanukkah2024/LotterySection/LotterySection.js
+++ src/components/Festivals/Hanukkah2024/LotterySection/LotterySection.js
@@ -9,11 +9,11 @@ function LotterySection() {
             <Heading>Лотерея</Heading>
             <div className="lotteryNew_ordered_circles"></div>
             <div className="lotteryNew_savivon"></div>
-            {/*<div className="lotteryNew_rounds"></div>
-            <div className="lotteryNew_hanukia"></div>*/}
+            <div className="lotteryNew_circles"></div>
+            <div className="lotteryNew_hanukia"></div>
             <div className='lotteryNew_text_container'>
                 <h3 className='lotteryNew_text lotteryNew_text_bold'>Благотворительная лотерея на фестивале "В Чемодане"</h3>
-                <p className='lotteryNew_text'>Фестиваль "В Чемодане" совместно с музеем еврейского народа АНУ организует благотворительную лотерею. Собранные средства будут направлены на помощь семьям, эвакуированным с юга и севера Израиля.</p>
+                <p className='lotteryNew_text lotteryNew_text_margin'>Фестиваль "В Чемодане" совместно с музеем еврейского народа АНУ организует благотворительную лотерею. Собранные средства будут направлены на помощь семьям, эвакуированным с юга и севера Израиля.</p>
 
                 <p className='lotteryNew_text'>Лоты для лотереи предоставлены:</p>
                 <ul>
@@ -25,11 +25,17 @@ function LotterySection() {
 
                 <p className='lotteryNew_text lotteryNew_text_bold'>На благотворительную лотерею нужно приобретать билеты заранее до 26 декабря включительно. Розыгрыш состоится 27 декабря 2024 г в 13.00 на фестивале в зале ТИШ.</p>
 
-                <h3 className='lotteryNew_title'>Как участвовать?</h3>
+                <h3 className='lotteryNew_title lotteryNew_title_star_container'>
+                    Как участвовать?
+                    <span className="lotteryNew_rounds"></span>
+                </h3>
                 <p className='lotteryNew_text'>Билеты можно приобрести онлайн по указанной ссылке во время фестиваля “В Чемодане” 26 и 27 декабря.</p>
                 <p className='lotteryNew_text'>Вы можете купить несколько билетов, увеличивая свои шансы на победу.</p>
 
-                <h3 className='lotteryNew_title'>Место и дата розыгрыша</h3>
+                <h3 className='lotteryNew_title lotteryNew_title_star_container'>
+                    Место и дата розыгрыша
+                    <span className="lotteryNew_star"></span>
+                </h3>
                 <p className='lotteryNew_text'>Дата: Пятница, 27 декабря 2024 года</p>
                 <p className='lotteryNew_text'>Время: 13.00</p>
                 <p className='lotteryNew_text'>Зал: ТИШ</p>
