b01bdfdc800299bba813a651d6e49bf5053851a0
Varvara Korableva <v.korableva1990@yandex.ru>
2024-12-01 19:29:27 +0200

    loterry page added

---
+++ src/pages/Hanukkah2024Page/LotteryHanukkah2024Page.js
@@ -0,0 +1,11 @@
+import LotteryHanukkah2024Pagination from '../../components/Festivals/Hanukkah2024/LotteryHanukkah2024Pagination/LotteryHanukkah2024Pagination'
+
+function LotteryHanukkah2024Page() {
+    return(
+        <>
+            <LotteryHanukkah2024Pagination/>
+        </>
+    )
+}
+
+export default LotteryHanukkah2024Page
