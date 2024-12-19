08c70de82f71d6933994df8e9442a7b24f362f69
Varvara Korableva <v.korableva1990@yandex.ru>
2024-12-19 23:24:45 +0200

    changed main text, changed pics

--- src/components/Main/MainAbout/AboutFectivalMain/AboutFectivalMain.js
+++ src/components/Main/MainAbout/AboutFectivalMain/AboutFectivalMain.js
@@ -14,17 +14,18 @@ function AboutFectivalMain() {
                                 <span className="aboutFectivalMain-subtitle-one-line"> НА РУССКОМ ЯЗЫКЕ</span> 
                                 <span className="aboutFectivalMain-subtitle-bold"> «В ЧЕМОДАНЕ»</span>
                                 <span>ЭТО</span>
+                                <span className="aboutFectivalMain-subtitle-bold">ГЛАВНОЕ КУЛЬТУРНОЕ СОБЫТИЕ</span>
                             </h3>
                             
                         </div>
                     <div className='aboutFectivalMain-stared-dots-pic'></div>
                 </div>
 
-                <p className="aboutFectivalMain-text aboutFectivalMain-text-first-p-margin">Книжный семейный фестиваль «В чемодане» – главное русскоязычное культурное событие в Израиле.</p>
+                <p className="aboutFectivalMain-text aboutFectivalMain-text-first-p-margin">Книжный семейный фестиваль <span className="aboutFectivalMain-subtitle-one-line">«В чемодане»</span> – главное русскоязычное культурное событие в Израиле</p>
                 <div className="aboutFectivalMain-dots-pic"></div>
                 <div className="aboutFectivalMain-flex-container-lamed">
                     <div className="aboutFectivalMain-letters-lamed"></div>
-                    <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">С сентября 2022 года фестиваль регулярно проводится в Тель-Авиве, Хайфе и Иерусалиме.</p>
+                    <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">С сентября 2022 года фестиваль регулярно проводится в Тель-Авиве, Хайфе и Иерусалиме</p>
                 </div>
                 <div className='aboutFectivalMain-flex-container'>
                     <div className='aboutFectivalMain-flex-book-letters-pics'></div>
@@ -37,25 +38,24 @@ function AboutFectivalMain() {
 
                     <div className="aboutFectivalMain-rounds-pic"></div>
                     <div className="aboutFectivalMain-third-container-text-container">
-                        <p className="aboutFectivalMain-text">«В Чемодане» единственный в Израиле фестиваль литературы на русском языке. К нам привозят свои книги независимые издательства из России, Европы и Израиля, а также местные книжные магазины.</p>
+                        <p className="aboutFectivalMain-text"><span className="aboutFectivalMain-subtitle-one-line">«В чемодане»</span> единственный в Израиле фестиваль литературы на русском языке. К нам привозят свои книги независимые издательства из России, Европы и Израиля, а также местные книжные магазины</p>
                         <div className="aboutFectivalMain-one-blue-round"></div>
                         <div className="aboutFectivalMain-orange-round-text-container">
                             <div className="aboutFectivalMain-one-orange-round"></div>
-                            <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">Фестиваль «В Чемодане» славится своими активностями: чтениями, круглыми столами, мастер-классами, лекциями, экскурсиями, концертами и театральными постановками для детей, подростков и взрослых.</p>
+                            <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">Фестиваль <span className="aboutFectivalMain-subtitle-one-line">«В чемодане»</span> славится своими активностями: чтениями, круглыми столами, мастер-классами, лекциями, экскурсиями, концертами и театральными постановками для детей, подростков и взрослых</p>
                         </div>
                     </div>
                     <div className="aboutFectivalMain-letters-pic"></div>
                 </div>
 
                 <div className="aboutFectivalMain-fourth-container">
-                    <div className="aboutFectivalMain-fourth-container-letter-pey"></div>
+                    {/*<div className="aboutFectivalMain-fourth-container-letter-pey"></div>*/}
                     
                     <div className="aboutFectivalMain-fourth-container-text-wrapper">
                         <div className="aboutFectivalMain-fourth-container-three-zigzags"></div>
                         <div className="aboutFectivalMain-fourth-container-text-container">
                             
-                            <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">Задача фестиваля — знакомить новых репатриантов с еврейской культурой и литературой через искусство. Важно, чтобы мероприятия фестиваля «В Чемодане» помогали участникам прочувствовать свою связь с еврейскими традициями и историей народа Израиля.
-</p>
+                            <p className="aboutFectivalMain-text aboutFectivalMain-shiftedText">Задача фестиваля — знакомить новых репатриантов с еврейской культурой и литературой через искусство. Важно, чтобы мероприятия фестиваля «В Чемодане» помогали участникам прочувствовать свою связь с еврейскими традициями и историей народа Израиля</p>
                         </div>
                     </div>
                         <div className="aboutFectivalMain-fourth-container-dots-pic"></div>
@@ -65,7 +65,7 @@ function AboutFectivalMain() {
                     </div>
                 </div>
                 <div className="aboutFectivalMain-fifth-container">
-                    <p className="aboutFectivalMain-text">Наши мероприятия становятся пространством для общения и обмена опытом, предоставляют площадку новым коллективам и творческим людям, помогает семьям чувствовать себя комфортно в привычном кругу, а также укреплять свою связь с еврейским наследием.
+                    <p className="aboutFectivalMain-text">Наши мероприятия становятся пространством для общения и обмена опытом, предоставляют площадку новым коллективам и творческим людям, помогает семьям чувствовать себя комфортно в привычном кругу, а также укреплять свою связь с еврейским наследием
 </p>
                     <div className="aboutFectivalMain-letters-container">
                         <div className="aboutFectivalMain-letter-dalet"></div>
@@ -74,10 +74,12 @@ function AboutFectivalMain() {
                     
                     <div className="aboutFectivalMain-letters-i-container">
                         <div className="aboutFectivalMain-letter-i"></div>
-                        <p className="aboutFectivalMain-text">С фестивалем «В чемодане» дает людям  моменты радости и уюта, несмотря на вызовы внешнего мира.</p>
+                        <p className="aboutFectivalMain-text">С фестивалем <span className="aboutFectivalMain-subtitle-one-line">«В чемодане»</span> дает людям  моменты радости и уюта, несмотря на вызовы внешнего мира</p>
                     </div>
 
                     <div className="aboutFectivalMain-open-book"></div>
+
+                    
                 </div>
             </div>
         </section>
