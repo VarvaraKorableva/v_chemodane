da9fcd97db363cb1cda67f6840f64d1abf9d6be7
Varvara Korableva <v.korableva1990@yandex.ru>
2025-02-26 11:56:54 +0200

    added icon in contact page

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,7 +1,6 @@
-import { FaInstagram, FaTelegram, FaFacebook } from 'react-icons/fa'
+import { FaInstagram, FaTelegram, FaFacebook, FaWhatsapp } from 'react-icons/fa'
 import Heading from '../../UI-Kit/Heading/Heading'
 import MarkupLayout from '../../components/Layout/MarkupLayout'
-import SeparatorPage from '../../components/SeparatorPage/SeparatorPage'
 import './ContactsPage.css'
 
 function ContactsPage() {
@@ -14,18 +13,23 @@ function ContactsPage() {
             <p className='ContactsPage__text ContactsPage__title'>Или любым другим способом:</p>
             <ul className="ContactsPage__links-container">
                 <li className='ContactsPage__link'>
-                    <a className='ContactsPage__contact-item-link' href='https://www.instagram.com/vchemodanefest/' target='_blank'>
+                    <a className='ContactsPage__contact-item-link' href='https://www.instagram.com/vchemodanefest/' target='_blank' rel="noopener noreferrer">
                         <FaInstagram />
                     </a>
                 </li>
                 <li className='ContactsPage__link'>
-                    <a className='ContactsPage__contact-item-link' href='https://www.facebook.com/vchemodanefest/' target='_blank'>
+                    <a className='ContactsPage__contact-item-link' href='https://www.facebook.com/vchemodanefest/' target='_blank' rel="noopener noreferrer">
                         <FaFacebook />
                     </a>
                 </li>
                 <li className='ContactsPage__link'>
-                    <a className='ContactsPage__contact-item-link' href='https://t.me/chemodanfest' target='_blank'>
+                    <a className='ContactsPage__contact-item-link' href='https://t.me/chemodanfest' target='_blank' rel="noopener noreferrer">
                         <FaTelegram />
+                    </a>
+                </li>
+                <li className='ContactsPage__link'>
+                    <a className='ContactsPage__contact-item-link' href="https://wa.me/+972534701675" target="_blank" rel="noopener noreferrer">
+                        <FaWhatsapp />
                     </a>
                 </li>
             </ul>
