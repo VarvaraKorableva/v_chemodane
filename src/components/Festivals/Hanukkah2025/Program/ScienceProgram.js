624a155700dce0756f62acb1e61d8998219ef7a2
Varvara Korableva <v.korableva1990@yandex.ru>
2025-11-21 21:38:17 +0200

    added section

--- src/components/Festivals/Hanukkah2025/Program/ScienceProgram.js
+++ src/components/Festivals/Hanukkah2025/Program/ScienceProgram.js
@@ -7,29 +7,35 @@ export default function ScienceProgram() {
     return(
         <Container as="section">
             
-            <Heading className="heading heading__3B6BB4">Научная программа</Heading>
+            <Heading className="heading heading__3B6BB4 scienceProgram__short__title">Научная программа</Heading>
 
             <div className='center_container_hanukkha2025'>
-                <ul className='textprogram_ul'>
+                <ul className='text program_ul'>
                     <li>
-                        <h3 className='uppercase'>Биологические мастер-классы от команды «Лаба в музее»</h3>
+                        <h3 className='uppercase scienceProgram__title__container'>Биологические мастер-классы от команды «Лаба в музее»</h3>
                         <p>
                         Занятия для разных возрастов — вас ждут запутанные детективные истории и научные эксперименты,
                         которые хочется повторить дома.
                         </p>
-                    </li>
 
-                    <li>
-                        <h3 className='uppercase'>Лекции о природе Израиля</h3>
+                        <div className='scienceProgram__first__right__pic'></div>
+                    </li >
+
+                    <li className='TheatreProgram__pics__container'>
+                        <h3 className='uppercase scienceProgram__title__container'>Лекции о природе Израиля</h3>
                         <p>— Орнитолог Роман Скибневский расскажет о птицах, их повадках и миграциях.</p>
                         <p>— Вероника Арсеньева — о растениях, которые мы встречаем каждый день, но редко знаем их историю.</p>
+
+                        <div className='scienceProgram__second__right__pic'></div>
                     </li>
 
                     <li>
-                        <h3 className='uppercase'>Интерактивная лекция о молекулах от Якова Журинского</h3>
+                        <h3 className='uppercase scienceProgram__title__container'>Интерактивная лекция о молекулах от Якова Журинского</h3>
                         <p>Простым и живым языком о том, из чего устроен наш мир.</p>
                     </li>
                 </ul>
+
+                <div className='scienceProgram__lastPic'></div>
             </div>
         </Container>
     )
