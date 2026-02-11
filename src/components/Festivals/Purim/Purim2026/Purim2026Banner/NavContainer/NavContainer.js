91b17c6c5a0d31bafa799622d240740a4f9b8a65
Varvara Korableva <v.korableva1990@yandex.ru>
2026-02-11 09:07:58 +0200

    new schedule and changed nav panel

--- src/components/Festivals/Purim/Purim2026/Purim2026Banner/NavContainer/NavContainer.js
+++ src/components/Festivals/Purim/Purim2026/Purim2026Banner/NavContainer/NavContainer.js
@@ -3,7 +3,7 @@ function NavContainer() {
         <nav className="hanukaTelAviv2024Banner__nav-container">
             <ul className="hanukaTelAviv2024Banner__ul-container">
                 <li className="hanukaTelAviv2024Banner__li">
-                    <a className="hanukaTelAviv2024Banner__link" href="#schedule_Hanukkah2025">Расписание</a>
+                    <a className="hanukaTelAviv2024Banner__link" href="#schedule_Purim2026">Расписание</a>
                 </li>
                 <li className="hanukaTelAviv2024Banner__li">
                     <a className="hanukaTelAviv2024Banner__link" href="#market">Участники маркета</a>
