94018cb7ac5d8223a418f67db5fc40264b9ed18d
Varvara Korableva <v.korableva1990@yandex.ru>
2026-03-13 21:46:09 +0200

    added banner

--- src/components/Events/EventPokaVseDoma/EventPokaVseDomaBanner/EventPokaVseDomaBanner.js
+++ src/components/Events/EventPokaVseDoma/EventPokaVseDomaBanner/EventPokaVseDomaBanner.js
@@ -5,6 +5,12 @@ function EventPokaVseDomaBanner() {
     return (
         <section className="EventPokaVseDomaBanner__section">
             <div className="EventPokaVseDomaBanner__pic-container">
+                <div className="EventPokaVseDomaBanner__title-container">
+                    <p className="EventPokaVseDomaBanner__title EventPokaVseDomaBanner__poka">Пока</p>
+                    <p className=" EventPokaVseDomaBanner__title EventPokaVseDomaBanner__vse">Все</p>
+                    <p className=" EventPokaVseDomaBanner__title EventPokaVseDomaBanner__doma">Дома</p>
+                    <p className="EventPokaVseDomaBanner__title EventPokaVseDomaBanner__onlain">он-лайн встречи</p>
+                </div>
             </div>
             <NavContainer/>
         </section>
